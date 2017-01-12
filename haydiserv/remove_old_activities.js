bc343071fcc41ec7e9cb49dd4b6f5d2b813529a0
naezith <tolgaay@live.com>
2017-01-12 12:56:51 +0300

    fx

--- haydiserv/remove_old_activities.js
+++ haydiserv/remove_old_activities.js
@@ -7,7 +7,7 @@ var database = require('mysql').createConnection({
 
 database.query('SELECT id FROM activity WHERE UNIX_TIMESTAMP(STR_TO_DATE(date,\'%d %m %Y\')) < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 1 DAY))', function(err, results, fields){
 	if(results.length == 0) process.exit();
-	var ids = results.map((obj) => { return obj['id']; });
+	var ids = results.map(function(obj) { return obj['id']; });
 	
 	
 	database.query('DELETE FROM activity WHERE id IN (?)', [ids], function(err, results, fields){
